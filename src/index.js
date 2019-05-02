f5866f5ef8f8903a1cb1d0953b274ca17b467b4d
Tyrel Chambers <tychambers3@gmail.com>
2019-05-02 19:34:46 -0400

    added logrocket

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LogRocket from 'logrocket';
+LogRocket.init('kstoxh/reddex');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
