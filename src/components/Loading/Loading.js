5464dbfe8159e399f65bf24d1f6f2c4e0a48ab87
Tyrel Chambers <tychambers3@gmail.com>
2019-05-02 19:38:47 -0400

    edit loading text

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -5,6 +5,7 @@ const Loading = (params) => {
   return (
     <div className="loading-wrapper d-f fxd-c">
       <h2 className="ta-c">Wrangling Reddit Posts... Hold Tight</h2>
+      <h4 className="ta-c">This might take a few seconds</h4>
       <div id="preloader">
         <div id="loader"></div>
       </div>
