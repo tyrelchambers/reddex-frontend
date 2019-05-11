a3b75f5d03fa64ed0b8263304ef1a1881f2d8f05
Tyrel Chambers <tychambers3@gmail.com>
2019-05-11 12:26:44 -0400

    add about page

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Header.scss';
+import Navbar from '../Navbar/Navbar';
 
 const Header = (props) => {
-  
+  return(
+    <header className="header">
+      <Navbar />
+    </header>
+  );
 }
 
 export default Header;
