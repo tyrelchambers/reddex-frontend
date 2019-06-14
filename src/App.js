be4ebe509853d00c19d18a5bba15388f578a018b
Tyrel Chambers <tychambers3@gmail.com>
2019-06-13 21:57:52 -0400

    add modal and sending messages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './app.scss';
 import PostFetch from './components/PostFetch/PostFetch';
+import { observer } from 'mobx-react-lite';
 
-function App() {
+const App = observer(() => {
+
   return (
     <div className="App w-100pr ml-a mr-a">
       <div className="mb-">
@@ -11,8 +13,9 @@ function App() {
         <p className="ta-c">In order to change current filters, you'll have to click "Reset Filters", then apply the filters you want</p>
       </div>
       <PostFetch />
+      
     </div>
   );
-}
+});
 
 export default App;
--- src/components/MessageAuthors/MessageAuthors.js
+++ src/components/MessageAuthors/MessageAuthors.js
@@ -1,20 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react'
 import './MessageAuthors.scss';
 import { observer } from 'mobx-react-lite';
-import UserStore from '../../stores/UserStore';
-import Axios from 'axios';
+import ModalStore from '../../stores/ModalStore';
+import ConfirmModal from '../ConfirmModal/ConfirmModal';
 
 const MessageAuthors = observer(({data, posts}) => {
   const [authorPosts, setAuthorPosts] = useState([]);
-  const store = useContext(UserStore);
-  const userId = store.getUser().uid;
-  const user = store.getUserProfile(userId);
+  const modalStore = useContext(ModalStore)
 
   const mockUser = 'ChapStique43';
 
   useEffect(() => {
     findPostsFromSelected(data, posts, setAuthorPosts);
   }, [data]);
+  
   return (
     <div className="message-author-box mt+ mb+">
       <div className="message-author-box-header">
@@ -22,18 +21,13 @@ const MessageAuthors = observer(({data, posts}) => {
       </div>
 
       <div className="message-author-body p-">
-        <div>
-          <h4 className="mb+">Message Preview</h4>
-
-          <h5 className="mb+">Subject</h5>
-          <p className="default-message-holder mw-500 lh-1-8">{console.log(posts)}</p>
-          <p className="default-message-holder mw-500 lh-1-8" id="defaultMessageHolder">
-            {user.defaultMessage}
-          </p>
-          <p className="subtle mt+ mb+">Please remember, you're sending a message to multiple people, use this tool responsibly.</p>
-          <button className="btn btn-primary" onClick={() => sendMessageToAuthors(mockUser)}>Send Message(s)</button>
-        </div>
+        <p className="subtle mt+ mb+">Confirming messages will open a pop-up that will walk you through each message to make sure it's correct. It will not send any messages.</p>
+        <button className="btn btn-primary" onClick={() => modalStore.setIsOpen(true)}>Confirm Messages</button>
       </div>
+      <ConfirmModal 
+        isOpen={modalStore.isOpen}
+        data={authorPosts}
+      />
     </div>
   )
 })
@@ -52,26 +46,7 @@ const findPostsFromSelected = (data, posts, setAuthorPosts) => {
   setAuthorPosts([...results]);
 }
 
-const sendMessageToAuthors = async (author) => {
-  const tokens = JSON.parse(window.localStorage.getItem('reddit_tokens')).access_token;
-  const subject = "Test message";
-  const link = `https://oauth.reddit.com/api/compose`;
-  const msg = window.localStorage.getItem("default_message");
-  const body = new FormData();
-  body.set('to', `/u/${author}`);
-  body.set("subject", subject);
-  body.set("text", msg);
 
-  await Axios.post(link, body, {
-    headers: {
-      "Authorization": `bearer ${tokens}`,
-      "Content-Type": "application/x-www-form-urlencoded"
-    }
-  })
-  .then(console.log)
-  .catch(console.log);
-  
-}
 
 
 export default MessageAuthors;
