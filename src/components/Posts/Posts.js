7f99d9b03a51212b417153f0fcb5a772f14d1cdc
Tyrel Chambers <tychambers3@gmail.com>
2019-06-27 19:53:52 -0400

    for branch deploy

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Posts.scss';
+import SubredditPost from '../SubredditPost/SubredditPost';
+
+export default function Posts({posts, loading, selectedPosts, setPosts, selectPost}) {
+  if ( posts.length > 0 && !loading ) {
+    return (
+      <ul className="post-list d-f ">
+        {posts.slice(0, 40).map(x => {
+          return(
+            <SubredditPost 
+              key={x.id} 
+              x={x}
+              setPosts={setPosts}
+              onClick={selectPost}
+              selectedPosts={selectedPosts}
+            />
+          ) 
+        })}
+      </ul>
+    )
+  } else {
+    return null;
+  }
+}
