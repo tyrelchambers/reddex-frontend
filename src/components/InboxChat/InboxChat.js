37ca7f413a0e8d09a5da450ca606cb330dcafcfc
Tyrel Chambers <tychambers3@gmail.com>
2019-08-10 18:11:51 -0400

    add form actions

--- src/components/InboxChat/InboxChat.js
+++ src/components/InboxChat/InboxChat.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import moment from 'moment';
 import dateFns from 'date-fns';
 import './InboxChat.scss';
+import '../Buttons/buttons.scss';
 import SendChatForm from '../Forms/SendChatForm';
 
 const InboxChat = ({data}) => {
@@ -20,10 +21,33 @@ const InboxChat = ({data}) => {
     return(
       <li key={id} className="chat">
         <h4 className={`chat-author ${isCurrent ? "chat-right" : ""}`}>{isCurrent ? "You" : x.author}</h4>
-        <p className={`chat-body ${isCurrent ? "chat-body-light" : "chat-body-dark"}`}>
-          {x.body}
-        </p>
-        <p className={`chat-date ${isCurrent ? "chat-right" : ""}`}>{dateFns.format(moment.unix(x.created_utc)._d, "MMM DD, YYYY h:mm:ss aa")}</p>
+        <div className={`chat-body-wrapper  ${isCurrent ? "right" : ""}`}>
+          <p className={`chat-body ${isCurrent ? "chat-body-light" : "chat-body-dark"}`}>
+            {x.body}
+          </p>
+
+          
+        </div>
+        <div className="d-f ai-c">
+          <p className={`chat-date ${isCurrent ? "chat-right" : ""}`}>{dateFns.format(moment.unix(x.created_utc)._d, "MMM DD, YYYY h:mm:ss aa")}</p>
+          <div className="chat-actions d-f">
+            <div className="chat-action-toggle" >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div className="chat-action-btn-wrapper d-f">
+              <button className="chat-action primary ai-c">
+                <i className="fas fa-bookmark mr-"></i>
+                Add to Read List
+              </button>
+              <button className="chat-action danger">
+                <i className="fas fa-trash mr-"></i>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
       </li>
     )
   })
