c508f7a9336e0156877b5ddc6da80ba5763f6050
Tyrel Chambers <tychambers3@gmail.com>
2019-08-13 20:18:38 -0400

    slim nav

--- src/layouts/DashboardNav/DashboardNav.js
+++ src/layouts/DashboardNav/DashboardNav.js
@@ -1,37 +1,34 @@
 import React from 'react'
 import './DashboardNav.scss';
 import {NavLink, Link} from 'react-router-dom';
-import brandImg from '../../assets/reddex-logo.svg';
 
 const DashboardNav = () => {
   return (
-    <nav className="dash-nav ">
-      <Brand
-        img={brandImg}
-      />
+    <nav className="dash-nav" id="dashNav">
+      <NavToggle />
       <ul >
         <li>
           <NavLink to="/dashboard/home" className="dash-nav-item" activeClassName="dash-nav-item-active ">
             <i className="fas fa-home"></i>
-            Home
+            <p>Home</p>
           </NavLink>
         </li>
         <li>
           <NavLink to="/dashboard/reading_list" className="dash-nav-item" activeClassName="dash-nav-item-active ">
             <i className="fas fa-file-alt"></i>
-            Stories
+            <p>Stories</p>
           </NavLink>
         </li>
         <li>
           <NavLink to="/dashboard/inbox" className="dash-nav-item" activeClassName="dash-nav-item-active ">
             <i className="fas fa-inbox"></i>
-            Inbox
+            <p>Inbox</p>
           </NavLink>
         </li>
         <li>
           <Link to="/" className="dash-nav-item">
             <i className="fas fa-download"></i>
-            Get Stories
+            <p>Get Stories</p>
           </Link>
         </li>
       </ul>
@@ -39,10 +36,13 @@ const DashboardNav = () => {
   )
 }
 
-const Brand = ({img}) => {
+const NavToggle = () => {
   return (
-    <div className="d-f ai-c jc-c mt-">
-      <img src={img} alt="Brand" style={{width: "130px"}}/>
+    <div className="d-f fxd-c jc-sb dash-nav-toggle" onClick={(e) => {
+      document.querySelector("#dashNav").classList.toggle('collapsed');
+      e.target.classList.toggle('closed')
+    }}>
+      <i className="fas fa-angle-double-right"></i>
     </div>
   );
 }
