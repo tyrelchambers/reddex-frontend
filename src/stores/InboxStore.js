5fb17148a8d918ebb84db55f8167cb86748d0256
Tyrel Chambers <tychambers3@gmail.com>
2019-08-22 18:34:48 -0400

    add after

--- src/stores/InboxStore.js
+++ src/stores/InboxStore.js
@@ -3,14 +3,18 @@ import { observable, action, decorate, toJS } from 'mobx';
 class InboxStore {
   messages = []
   selectedMessage = {}
-  
+  latestAfter = ""
+
   setMessages(data) {
     this.messages = data.data;
+    this.latestAfter = data.after;
   }
 
   getMessages() {
     return toJS(this.messages);
   }
+
+
 
   setSelectedMessage(data) {
     this.selectedMessage = data;
@@ -25,7 +29,8 @@ decorate(InboxStore, {
   messages: observable,
   setMessages: action,
   selectedMessage: observable,
-  setSelectedMessage: action
+  setSelectedMessage: action,
+  latestAfter: observable
 });
 
 export default new InboxStore();
