1cb56d636ce1fb363326ffc032ae2de7a1a4576b
Tyrel Chambers <tychambers3@gmail.com>
2019-08-24 19:13:22 -0400

    add announcement

--- src/layouts/DisplayWrapper/DisplayWrapper.js
+++ src/layouts/DisplayWrapper/DisplayWrapper.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Header from '../Header/Header';
+import Announcement from '../Announcement/Announcement';
 
 const DisplayWrapper = (props) => {
-  if ( props.hasHeader ) {
+  if ( props.hasHeader === true ) {
     return (
-      <div className={props.className}>
+      <div>
         <Header />
-        {props.children}
+        <Announcement 
+          title={props.announcementTitle}
+          body={props.announcementBody}
+        />
+        <div className={props.className}>        
+          {props.children}
+        </div>
       </div>
     )
   } else {
--- src/layouts/UsedBy/UsedBy.jsx
+++ src/layouts/UsedBy/UsedBy.jsx
@@ -4,7 +4,6 @@ import './UsedBy.scss';
 const UsedBy = () => {
   return (
     <div className="used-by-wrapper">
-      <h3>Used By:</h3>
       <ul className="used-by-list">
         <li className="used-by-item">Dark Wolf Reads</li>
         <li className="used-by-item">Dead Leaf Clover</li>
