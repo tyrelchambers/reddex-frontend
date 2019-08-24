a696653f689dcd84eaca90e1f48043306d440743
Tyrel Chambers <tychambers3@gmail.com>
2019-08-24 09:26:19 -0400

    refactor some code

--- src/layouts/DashboardDropdown/DashboardDropdown.jsx
+++ src/layouts/DashboardDropdown/DashboardDropdown.jsx
@@ -5,18 +5,36 @@ import { inject } from 'mobx-react';
 import { observer } from 'mobx-react-lite';
 
 const DashboardDropdown = inject("UserStore")(observer(({UserStore}) => {
+  const resetVisitorStatus = () =>{
+    window.localStorage.setItem("new_visitor", null);
+  }
   return (
-    <ul className="dashboard-dropdown d-f fxd-c">
-      <li>
-        <Link to="/account/default_message" className="dropdown-link">Account</Link>
-      </li>
-      
-      <li>
-        <Link to="/" className="dropdown-link" onClick={() => {
-          UserStore.signOut()
-        }}>Sign Out</Link>
-      </li>
-    </ul>
+    <div className="dropdown-wrapper">
+      <ul className="dashboard-dropdown d-f fxd-c">
+       <li className="d-f ai-c ">
+          <Link to="/dashboard/home"
+            className="dropdown-link"
+          >Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/account/default_message" className="dropdown-link">Account</Link>
+        </li>
+        
+        <li className="d-f ai-c ">
+          <Link onClick={() => {
+            resetVisitorStatus()
+          }} 
+          to="#"
+          className="dropdown-link">Reset Visitor Status</Link>
+        </li>
+
+        <li>
+          <Link to="/" className="dropdown-link" onClick={() => {
+            UserStore.signOut()
+          }}>Sign Out</Link>
+        </li>
+      </ul>
+    </div>
   )
 }));
 
--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './Header.scss';
 import Navbar from '../Navbar/Navbar';
+import reddexLogo from '../../assets/reddex-logo.svg';
+import {inject, observer } from 'mobx-react';
 
-const Header = (props) => {
+const Header = inject("UserStore")(observer(({UserStore}) => {
   return(
-    <header className="header">
-      <Navbar />
+    <header className="header d-f jc-sb">
+      <div className="brand">
+        <img src={reddexLogo} alt="Reddex"/>
+      </div>
+      <Navbar 
+        redditProfile={UserStore.getRedditProfile()}
+      />
     </header>
   );
-}
+}));
 
 export default Header;
