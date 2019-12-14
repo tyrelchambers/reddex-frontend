1375757079ed9f00f958a2a622c30bd54173f8f6
Tyrel Chambers <tychambers3@gmail.com>
2019-12-14 08:42:03 -0500

    add tab nav for readingl ist

--- src/Pages/Dashboard/ReadingList/ReadingList.jsx
+++ src/Pages/Dashboard/ReadingList/ReadingList.jsx
@@ -5,11 +5,10 @@ import ReadingListDumb from './ReadingListDumb';
 import { inject } from 'mobx-react';
 import { observer } from 'mobx-react-lite';
 import Axios from 'axios';
-import HR from '../../../components/HR/HR';
 import CompletedStories from './CompletedStories';
-import ReadingListStore from '../../../stores/ReadingListStore';
+import { NavLink } from 'react-router-dom';
 
-const ReadingList = inject("ReadingListStore")(observer(({ReadingListStore}) => {  
+const ReadingList = inject("ReadingListStore")(observer(({ReadingListStore, location}) => {  
   useEffect(() => {
     const token = window.localStorage.getItem('token');
 
@@ -17,26 +16,53 @@ const ReadingList = inject("ReadingListStore")(observer(({ReadingListStore}) => 
     getCompletedStoriesFromDb(token, ReadingListStore);
   }, []);
 
+  const params = new URLSearchParams(window.location.search);
+
+  const Tabs = () => (
+    <ul className="tabs-wrapper">
+      <li className="tabs-item">
+        <NavLink to="/dashboard/reading_list?t=open" activeClassName="tab-item-active" isActive={() => {
+          if (params.get('t') === "open") {
+            return true;
+          }
+        }}>Accepted</NavLink>
+      </li>
+      <li className="tabs-item">
+        <NavLink to="/dashboard/reading_list?t=completed" activeClassName="tab-item-active" isActive={() => {
+          if ( params.get('t') === "completed" ) {
+            return true
+          }
+        }}>Completed</NavLink>
+      </li>
+    </ul>
+  )
+
   return (
     <Dashboard>
-
+      <Tabs />
       <div className="d-f mobile-column">
-        <ReadingListDumb 
-          list={ReadingListStore.getToRead()}
-          setExpanded={(v) => ReadingListStore.setDim(v)}
-          callback={(v) => ReadingListStore.transferStoryFromList(v, "toRead", "completed")}
-        />
+        {params.get('t') === "open" &&
+          <ReadingListDumb 
+            list={ReadingListStore.getToRead()}
+            setExpanded={(v) => ReadingListStore.setDim(v)}
+            callback={(v) => ReadingListStore.transferStoryFromList(v, "toRead", "completed")}
+          />
+        }
 
-        <CompletedStories 
-          list={ReadingListStore.getCompleted()}
-          ReadingListStore={ReadingListStore}
-          callback={(v) => ReadingListStore.transferStoryFromList(v, "completed", "toRead")}
-          removeStoryFromDb={removeStoryFromDb}
-        />
+        {params.get('t') === "completed" &&   
+          <CompletedStories 
+            list={ReadingListStore.getCompleted()}
+            ReadingListStore={ReadingListStore}
+            callback={(v) => ReadingListStore.transferStoryFromList(v, "completed", "toRead")}
+            removeStoryFromDb={removeStoryFromDb}
+          />
+        }
       </div>
     </Dashboard>
   )
 }));
+
+
 
 const getCompletedFromDb = (token, store) => {
   Axios.get(`${process.env.REACT_APP_BACKEND}/api/profile/reading_list?permission=true`, {
