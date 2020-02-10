fccd1d8ed6116287489cd784421a7bdd0765c9fc
Tyrel Chambers <tychambers3@gmail.com>
2020-02-09 21:02:36 -0500

    add basic get for submitted stories

--- src/Pages/SubmittedStories/SubmittedStories.jsx
+++ src/Pages/SubmittedStories/SubmittedStories.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './SubmittedStories.scss'
 import Dashboard from '../Dashboard/Dashboard'
+import { getAxios } from '../../api';
 
 export default function SubmittedStories() {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const fn = async () => {
+      await getAxios({
+        url: '/submitted/'
+      }).then(res => {
+        if (res) {
+          setState([...res])
+        }
+      })
+    }
+
+    fn()
+  }, []);
+
   return (
     <Dashboard>
       <h1>Submitted Stories</h1>
+      
     </Dashboard>
   )
 }
