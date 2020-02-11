051b8db30dc775899a778af771d382af5b66028b
Tyrel Chambers <tychambers3@gmail.com>
2020-02-11 11:26:39 -0500

    sort submitted stories

--- src/Pages/SubmittedStories/SubmittedStories.jsx
+++ src/Pages/SubmittedStories/SubmittedStories.jsx
@@ -4,11 +4,11 @@ import Dashboard from '../Dashboard/Dashboard'
 import { getAxios } from '../../api';
 import SubmittedItem from '../../components/SubmittedItem/SubmittedItem';
 import { inject, observer } from 'mobx-react';
-import { Modal } from '../../components/Modal/Modal';
 import moment from 'moment';
 
 const SubmittedStories = inject("ModalStore")(observer(({ModalStore}) => {
   const [state, setState] = useState([]);
+  const [ sortVal, setSortVal ] = useState("");
 
   useEffect(() => {
     const fn = async () => {
@@ -24,12 +24,27 @@ const SubmittedStories = inject("ModalStore")(observer(({ModalStore}) => {
     fn()
   }, []);
 
-  const stories = state.sort((a, b) => moment(a.created_at).valueOf() - moment(b.created_at).valueOf()).reverse().map((x, id) => <SubmittedItem data={x} key={id} />)
+  const stories = 
+    state
+      .filter(x => {
+        if (
+          x.story_title.toLowerCase().includes(sortVal.toLowerCase())
+          || x.author.toLowerCase().includes(sortVal.toLowerCase())
+          || x.tags.toLowerCase().includes(sortVal.toLowerCase())
+          ) {
+            return x;
+          }
+      })
+      .sort((a, b) => moment(a.created_at).valueOf() - moment(b.created_at).valueOf())
+      .reverse()
+      .map((x, id) => <SubmittedItem data={x} key={id} />)
 
   return (
     <Dashboard>
       <h1>Submitted Stories</h1>
 
+      <p className="font-bold mt+">Sort by title, author, or tags</p>
+      <input type="text" className="search-large w-100pr  mb+" placeholder="Search inbox by username..." onChange={e => setSortVal(e.target.value.toLowerCase())}/>  
       <div className="d-f submitted-headers">
         <p>Title</p>
         <p>Author</p>
