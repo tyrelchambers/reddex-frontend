ce6f4b7349257faef5092de142e21bd1ab83cb5b
Tyrel Chambers <tychambers3@gmail.com>
2020-03-09 10:26:11 -0400

    hide filters on mobile by default

--- src/components/SubredditFilters/SubredditFilters.js
+++ src/components/SubredditFilters/SubredditFilters.js
@@ -3,6 +3,7 @@ import './SubredditFilters.scss';
 import filterOptionsJSON from './filterOptions';
 import SelectField from '../SelectField/SelectField';
 import { toast } from 'react-toastify';
+import {MinimalButton} from '../Buttons/Buttons'
 
 const SubredditFilters = ({ setReloadPosts, posts, setPosts, reloadPosts}) => {
   const [ keywords, setKeywords ] = useState("");
@@ -11,36 +12,47 @@ const SubredditFilters = ({ setReloadPosts, posts, setPosts, reloadPosts}) => {
     upvotes: 0,
     operator: ">"
   });
+  const [collapsed, setCollapsed] = useState(document.body.clientWidth <= 425 ? true : false);
 
   return(
     <div className="d-f fxd-c w-100pr filters-wrapper">
-      <div className=" d-f" style={{height: '100%'}}>
-        <div className="d-f w-100pr ai-c inputs">
-          <SelectField 
-            data={filterOptionsJSON}
-            label="Greater than"
-            options={filterOptions}
-            setOptions={setFilterOptions}
-            prop="operator"
-          />
-          
-          <input type="number" className="input ml-" placeholder="Upvote Count (default: 0)" onChange={e => setFilterOptions({...filterOptions, upvotes: e.target.value})}/>
-          <input type="text" className="input ml-" placeholder="keywords separated by commas" onChange={(e) => setKeywords(e.target.value)}/>
+      {document.body.clientWidth <= 425 &&
+        <MinimalButton
+          onClick={() => setCollapsed(!collapsed)}
+          classNames="mb-- mt--"
+        >
+          {collapsed ? "Show Filters" : "Hide Filters"}
+        </MinimalButton>
+      }
+      {!collapsed &&
+        <div className=" d-f" style={{height: '100%'}}>
+          <div className="d-f w-100pr ai-c inputs">
+            <SelectField 
+              data={filterOptionsJSON}
+              label="Greater than"
+              options={filterOptions}
+              setOptions={setFilterOptions}
+              prop="operator"
+            />
+            
+            <input type="number" className="input ml-" placeholder="Upvote Count (default: 0)" onChange={e => setFilterOptions({...filterOptions, upvotes: e.target.value})}/>
+            <input type="text" className="input ml-" placeholder="keywords separated by commas" onChange={(e) => setKeywords(e.target.value)}/>
 
+          </div>
+
+          <div className="filter-actions d-f ai-c">
+            <button className={`btn btn-tiertiary ${filterOptions.seriesOnly ? "active" : ""}`} onClick={() => setFilterOptions({...filterOptions, seriesOnly: !filterOptions.seriesOnly})}>Series Only</button>
+
+            <button className="btn btn-tiertiary" onClick={() => {
+              resetFilters(setFilterOptions);
+              setReloadPosts(!reloadPosts);
+            }}>Reset Filters</button>
+            <button className="btn btn-secondary ml-" onClick={() => {
+              applyFilters(posts, setPosts, keywords, filterOptions.upvotes, filterOptions.operator, filterOptions.seriesOnly);
+            }}>Apply Filters</button>
+          </div>
         </div>
-
-        <div className="filter-actions d-f ai-c">
-          <button className={`btn btn-tiertiary ${filterOptions.seriesOnly ? "active" : ""}`} onClick={() => setFilterOptions({...filterOptions, seriesOnly: !filterOptions.seriesOnly})}>Series Only</button>
-
-          <button className="btn btn-tiertiary" onClick={() => {
-            resetFilters(setFilterOptions);
-            setReloadPosts(!reloadPosts);
-          }}>Reset Filters</button>
-          <button className="btn btn-secondary ml-" onClick={() => {
-            applyFilters(posts, setPosts, keywords, filterOptions.upvotes, filterOptions.operator, filterOptions.seriesOnly);
-          }}>Apply Filters</button>
-        </div>
-      </div>
+      }
     </div>
   );
 }
