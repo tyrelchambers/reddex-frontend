2eb64092a8d58c25ee338d64e0957075411b55f2
Tyrel Chambers <tychambers3@gmail.com>
2020-03-13 10:38:49 -0400

    removed sass variable colours

--- src/layouts/Preferences/Preferences.jsx
+++ src/layouts/Preferences/Preferences.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Preferences.scss'
+import themes from './themes'
 
 const Preferences = () => {
   const [theme, setTheme] = useState("" || localStorage.getItem('theme'));
@@ -12,31 +13,25 @@ const Preferences = () => {
   }
 
   const toggleTheme = () => {
-    if (theme === "dark") {
-      setThemeHandler('light')
-    }  else {
-      setThemeHandler('dark')
-    }
+    setThemeHandler(theme)
   }
 
-  const switcher = theme === 'dark' ? (
-    <button className="pref dark-mode-switch d-f ai-c" onClick={() => {
-      toggleTheme('light')
-    }}>
-      <i className="fas fa-adjust"></i>
-      <p className="dark-mode-on">Light mode</p>
-    </button>
-  ) : (
-    <button className="pref dark-mode-switch d-f ai-c" onClick={() => {
-      toggleTheme('dark')
-    }}>
-      <i className="fas fa-adjust flipped"></i>
-      <p>Dark mode</p>
-    </button>
-  )
+  const render = (theme) => {
+    return (
+      <button className="pref dark-mode-switch d-f ai-c" onClick={() => {
+        toggleTheme(theme)
+      }}>
+        {themes[theme].icon}
+        <p>{themes[theme].label}</p>
+      </button>
+    )
+  }
+
   return (
     <div className="d-f">
-      {switcher}
+      {Object.keys(themes).map((x, id) => (
+        console.log(x)
+      ))}
     </div>
   );
 }
---
+++ src/layouts/Preferences/themes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const themes = {
+  "dark": {
+    icon: <i className="fas fa-adjust flipped"></i>,
+    label: "Dark mode"
+  },
+  "light": {
+    icon: <i className="fas fa-adjust"></i>,
+    label: "Light mode"
+  }
+}
+
+export default themes;
