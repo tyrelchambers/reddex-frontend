56579a6174dd4d9311a609dd901294eeb21c76e8
Tyrel Chambers <tychambers3@gmail.com>
2020-05-29 14:27:56 -0400

    remove tweet button on every other page than gen settings

--- src/Webbuilder/Dashboard/Timelines/Timelines.jsx
+++ src/Webbuilder/Dashboard/Timelines/Timelines.jsx
@@ -17,10 +17,7 @@ const Timelines = ({SiteStore}) => {
         <WithNav 
           tabs={tabs}
           optionalTabs={SiteStore.preview.subdomain && SiteStore.isSiteSaved ? [
-            <a href={`https://${SiteStore.preview.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`} rel="noopener noreferrer" target="_blank" className="td-n">View your site</a>,
-            <div className="d-f ai-c share-wrapper">
-              <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-url={`https://${SiteStore.config.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`} data-text={`Check out my new webpage!`} data-via="ReddexApp" data-hashtags="newSite" data-show-count="false">Tweet</a>
-            </div>
+            <a href={`https://${SiteStore.preview.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`} rel="noopener noreferrer" target="_blank" className="td-n">View your site</a>
           ]:  []}
         >
           <Youtube />
