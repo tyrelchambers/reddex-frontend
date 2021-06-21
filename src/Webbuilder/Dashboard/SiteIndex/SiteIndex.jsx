dc5612b3fce8801521cf9472df329b8a0665134c
Tyrel Chambers <tychambers3@gmail.com>
2021-06-21 19:09:54 -0400

    site builder

--- src/Webbuilder/Dashboard/SiteIndex/SiteIndex.jsx
+++ src/Webbuilder/Dashboard/SiteIndex/SiteIndex.jsx
@@ -1,65 +1,91 @@
-import React, { useEffect, useRef } from 'react'
-import './SiteIndex.scss'
-import Dashboard from '../../../Pages/Dashboard/Dashboard'
-import SiteBuilderForm from '../../../components/Forms/SiteBuilderForm'
-import { inject } from 'mobx-react'
-import { observer } from 'mobx-react-lite'
-import ToggleStatus from '../../../components/ToggleStatus/ToggleStatus'
-import { toast } from 'react-toastify'
-import HR from '../../../components/HR/HR'
-import { MainButton } from '../../../components/Buttons/Buttons'
-import tabs from '../tabs';
-import {getAxios } from '../../../api/index'
-import WithNav from '../../../layouts/WithNav/WithNav'
-import SiteBuilderWrapper from '../../../layouts/SiteBuilderWrapper/SiteBuilderWrapper'
-import { H1, H2, H1Subtitle, H2Subtitle } from '../../../components/Headings/Headings'
+import React, { useEffect, useRef } from "react";
+import "./SiteIndex.scss";
+import Dashboard from "../../../Pages/Dashboard/Dashboard";
+import SiteBuilderForm from "../../../components/Forms/SiteBuilderForm";
+import { inject } from "mobx-react";
+import { observer } from "mobx-react-lite";
+import ToggleStatus from "../../../components/ToggleStatus/ToggleStatus";
+import { toast } from "react-toastify";
+import HR from "../../../components/HR/HR";
+import { MainButton } from "../../../components/Buttons/Buttons";
+import tabs from "../tabs";
+import { getAxios } from "../../../api/index";
+import WithNav from "../../../layouts/WithNav/WithNav";
+import SiteBuilderWrapper from "../../../layouts/SiteBuilderWrapper/SiteBuilderWrapper";
+import {
+  H1,
+  H2,
+  H1Subtitle,
+  H2Subtitle,
+} from "../../../components/Headings/Headings";
 
-const SiteIndex = inject("SiteStore", "FormStore")(observer(({SiteStore, FormStore}) => {
-  const pondRef = useRef()
+const SiteIndex = inject(
+  "SiteStore",
+  "FormStore"
+)(
+  observer(({ SiteStore, FormStore }) => {
+    const pondRef = useRef();
 
-  useEffect(() => {
-    window.twttr.widgets.load(
-      document.querySelector(".share-wrapper")
-    );
-  }, []);
+    useEffect(() => {
+      window.twttr.widgets.load(document.querySelector(".share-wrapper"));
+    }, []);
 
-  const configHandler = (e) => {
-    SiteStore.setConfig({[e.target.name]: e.target.value})
-  }
+    const configHandler = (e) => {
+      SiteStore.setConfig({ [e.target.name]: e.target.value });
+    };
 
-  const activateSiteHandler = async () => {
-    await getAxios({
-      url: '/site/activate',
-      method: 'post'
-    })
-    .then(res => {
-      if (res) {
-        SiteStore.setInitial(res.website)
-        FormStore.setOptionsId(res.options.uuid)
-        SiteStore.setActivated(true)
-      }
-    });
-    
-    toast.success("Site activated")
-  }
+    const activateSiteHandler = async () => {
+      await getAxios({
+        url: "/site/activate",
+        method: "post",
+      }).then((res) => {
+        if (res) {
+          SiteStore.setInitial(res.website);
+          FormStore.setOptionsId(res.options.uuid);
+          SiteStore.setActivated(true);
+        }
+      });
 
-  
+      toast.success("Site activated");
+    };
+
     return (
       <Dashboard>
         <H1>Site Builder</H1>
         <H1Subtitle>Build your website and advertise what you do.</H1Subtitle>
+        <WithNav
+          tabs={SiteStore.activated ? tabs : []}
+          optionalTabs={
+            SiteStore.preview.subdomain && SiteStore.isSiteSaved
+              ? [
+                  <a
+                    href={`https://${SiteStore.preview.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="td-n"
+                  >
+                    View your site
+                  </a>,
+                  <div className="d-f ai-c share-wrapper">
+                    <a
+                      href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+                      className="twitter-share-button"
+                      data-url={`https://${SiteStore.config.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`}
+                      data-text={`Check out my new webpage!`}
+                      data-via="ReddexApp"
+                      data-hashtags="newSite"
+                      data-show-count="false"
+                    >
+                      Tweet
+                    </a>
+                  </div>,
+                ]
+              : []
+          }
+        >
           <SiteBuilderWrapper>
-            <WithNav 
-              tabs={SiteStore.activated ? tabs : []}
-              optionalTabs={SiteStore.preview.subdomain && SiteStore.isSiteSaved ? [
-                <a href={`https://${SiteStore.preview.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`} rel="noopener noreferrer" target="_blank" className="td-n">View your site</a>,
-                <div className="d-f ai-c share-wrapper">
-                  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-url={`https://${SiteStore.config.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`} data-text={`Check out my new webpage!`} data-via="ReddexApp" data-hashtags="newSite" data-show-count="false">Tweet</a>
-                </div>
-              ]:  []}
-            >
-           
-              {!SiteStore.activated &&
+            <div className="max-w-2xl">
+              {!SiteStore.activated && (
                 <div className="mt- d-f ai-c">
                   <p className="mr-">Activate website</p>
                   <ToggleStatus
@@ -69,12 +95,12 @@ const SiteIndex = inject("SiteStore", "FormStore")(observer(({SiteStore, FormSto
                     toggled={SiteStore.activated ? true : false}
                   />
                 </div>
-              }
-              {SiteStore.activated &&
-                <section  className="site-general-wrapper">
+              )}
+              {SiteStore.activated && (
+                <section className="site-general-wrapper">
                   <H2>General Settings</H2>
                   <div className="mt+">
-                    <SiteBuilderForm 
+                    <SiteBuilderForm
                       configHandler={configHandler}
                       config={SiteStore.config}
                       pondRef={pondRef}
@@ -82,25 +108,30 @@ const SiteIndex = inject("SiteStore", "FormStore")(observer(({SiteStore, FormSto
                     />
                   </div>
 
-                  <HR/>
+                  <HR />
 
                   <H2>Danger Zone</H2>
-                  <H2Subtitle>This is permanent. If you delete your site, you can create it again, but everything will be lost.</H2Subtitle>
+                  <H2Subtitle>
+                    This is permanent. If you delete your site, you can create
+                    it again, but everything will be lost.
+                  </H2Subtitle>
                   <MainButton
                     value="Delete Site"
                     className="btn btn-tiertiary danger mt-"
-                    onClick={() => SiteStore.deleteSiteHandler(SiteStore.config.uuid)}
+                    onClick={() =>
+                      SiteStore.deleteSiteHandler(SiteStore.config.uuid)
+                    }
                   >
                     <i className="fas fa-trash"></i>
-                    
                   </MainButton>
                 </section>
-              }
-          </WithNav>
-        </SiteBuilderWrapper>
+              )}
+            </div>
+          </SiteBuilderWrapper>
+        </WithNav>
       </Dashboard>
-    ) 
-  
-}));
+    );
+  })
+);
 
 export default SiteIndex;
--- src/Webbuilder/Dashboard/Timelines/Timelines.jsx
+++ src/Webbuilder/Dashboard/Timelines/Timelines.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import Youtube from './Youtube/Youtube';
-import Twitter from './Twitter/Twitter'
-import Dashboard from '../../../Pages/Dashboard/Dashboard';
-import WithNav from '../../../layouts/WithNav/WithNav';
-import SiteBuilderWrapper from '../../../layouts/SiteBuilderWrapper/SiteBuilderWrapper';
-import tabs from '../tabs'
-import { inject, observer } from 'mobx-react';
-import { H1, H1Subtitle } from '../../../components/Headings/Headings';
+import React from "react";
+import Youtube from "./Youtube/Youtube";
+import Twitter from "./Twitter/Twitter";
+import Dashboard from "../../../Pages/Dashboard/Dashboard";
+import WithNav from "../../../layouts/WithNav/WithNav";
+import SiteBuilderWrapper from "../../../layouts/SiteBuilderWrapper/SiteBuilderWrapper";
+import tabs from "../tabs";
+import { inject, observer } from "mobx-react";
+import { H1, H1Subtitle } from "../../../components/Headings/Headings";
 
-const Timelines = ({SiteStore}) => {
+const Timelines = ({ SiteStore }) => {
   return (
     <Dashboard>
       <H1>Site Builder</H1>
       <H1Subtitle>Build your website and advertise what you do.</H1Subtitle>
-      <SiteBuilderWrapper>
-        <WithNav 
-          tabs={tabs}
-          optionalTabs={SiteStore.preview.subdomain && SiteStore.isSiteSaved ? [
-            <a href={`https://${SiteStore.preview.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`} rel="noopener noreferrer" target="_blank" className="td-n">View your site</a>
-          ]:  []}
-        >
+      <WithNav
+        tabs={tabs}
+        optionalTabs={
+          SiteStore.preview.subdomain && SiteStore.isSiteSaved
+            ? [
+                <a
+                  href={`https://${SiteStore.preview.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="td-n"
+                >
+                  View your site
+                </a>,
+              ]
+            : []
+        }
+      >
+        {" "}
+        <SiteBuilderWrapper>
           <Youtube />
-          <Twitter/>
-        </WithNav>
-      </SiteBuilderWrapper>
+          <Twitter />
+        </SiteBuilderWrapper>
+      </WithNav>
     </Dashboard>
   );
-}
+};
 
 export default inject("SiteStore")(observer(Timelines));
