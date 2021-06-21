cfb81852b81128456c0ecb8e6d69ecb302fb12b1
Tyrel Chambers <tychambers3@gmail.com>
2021-06-21 19:11:41 -0400

    .

--- src/Webbuilder/Dashboard/Theme/Theme.jsx
+++ src/Webbuilder/Dashboard/Theme/Theme.jsx
@@ -1,40 +1,48 @@
-import React from 'react';
-import SiteBuilderThemeForm from '../../../components/Forms/SiteBuilderThemeForm';
-import { observer, inject } from 'mobx-react';
-import Dashboard from '../../../Pages/Dashboard/Dashboard';
-import WithNav from '../../../layouts/WithNav/WithNav';
-import SiteBuilderWrapper from '../../../layouts/SiteBuilderWrapper/SiteBuilderWrapper';
-import { H2, H1, H1Subtitle } from '../../../components/Headings/Headings';
-import tabs from '../tabs'
+import React from "react";
+import SiteBuilderThemeForm from "../../../components/Forms/SiteBuilderThemeForm";
+import { observer, inject } from "mobx-react";
+import Dashboard from "../../../Pages/Dashboard/Dashboard";
+import WithNav from "../../../layouts/WithNav/WithNav";
+import SiteBuilderWrapper from "../../../layouts/SiteBuilderWrapper/SiteBuilderWrapper";
+import { H2, H1, H1Subtitle } from "../../../components/Headings/Headings";
+import tabs from "../tabs";
 
-const Theme = ({SiteStore}) => {
-
+const Theme = ({ SiteStore }) => {
   const configHandler = (e) => {
-    SiteStore.setConfig({[e.target.name]: e.target.value})
-  }
+    SiteStore.setConfig({ [e.target.name]: e.target.value });
+  };
 
   return (
     <Dashboard>
       <H1>Site Builder</H1>
       <H1Subtitle>Build your website and advertise what you do.</H1Subtitle>
-      <WithNav 
+      <WithNav
         tabs={tabs}
-        optionalTabs={SiteStore.preview.subdomain && SiteStore.isSiteSaved ? [
-          <a href={`https://${SiteStore.preview.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`} rel="noopener noreferrer" target="_blank" className="td-n">View your site</a>
-        ]:  []}
+        optionalTabs={
+          SiteStore.preview.subdomain && SiteStore.isSiteSaved
+            ? [
+                <a
+                  href={`https://${SiteStore.preview.subdomain}.${process.env.REACT_APP_SUBDOMAIN_HOST}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="td-n"
+                >
+                  View your site
+                </a>,
+              ]
+            : []
+        }
       >
+        <H2 className="mb-6">Colour Theme</H2>
         <SiteBuilderWrapper>
-          <H2>Colour Theme</H2>
-          <div className="mt+">
-            <SiteBuilderThemeForm 
-              configHandler={configHandler}
-              config={SiteStore.config}
-            />
-          </div>
+          <SiteBuilderThemeForm
+            configHandler={configHandler}
+            config={SiteStore.config}
+          />
         </SiteBuilderWrapper>
       </WithNav>
     </Dashboard>
   );
-}
+};
 
 export default inject("SiteStore")(observer(Theme));
