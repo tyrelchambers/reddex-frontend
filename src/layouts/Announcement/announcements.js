87f52967c27e6bbaa7aa57815d4a63cf0836915c
Tyrel Chambers <tychambers3@gmail.com>
2021-07-25 10:24:12 -0400

    remove ko-fi

--- src/layouts/Announcement/announcements.js
+++ src/layouts/Announcement/announcements.js
@@ -1,21 +1,34 @@
-import React from "react"
+import React from "react";
 
 export default {
   basic: {
     title: "Getting Started",
-    body: "Enter the subreddit you want to search, in the subreddit field. Then click 'Get Posts'"
+    body: "Enter the subreddit you want to search, in the subreddit field. Then click 'Get Posts'",
   },
   productHunt: {
     title: "We're on the internet!",
-    body: <a href="https://www.producthunt.com/posts/reddex?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-reddex" target="_blank" rel="noopener noreferrer"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=181398&theme=light" alt="Reddex - A Toolkit for Youtube Reddit Narrators | Product Hunt Embed" style={{width: "250px", height: "54px"}} width="250px" height="54px" /></a>
-    
+    body: (
+      <a
+        href="https://www.producthunt.com/posts/reddex?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-reddex"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=181398&theme=light"
+          alt="Reddex - A Toolkit for Youtube Reddit Narrators | Product Hunt Embed"
+          style={{ width: "250px", height: "54px" }}
+          width="250px"
+          height="54px"
+        />
+      </a>
+    ),
   },
   patreon: {
     title: "Finding Reddex helpful?",
-    body: `Support Reddex via our new Patreon! Please note, you'll never have to pay to use Reddex. This is strictly to help the site!`
+    body: `Support Reddex via our Patreon!`,
   },
   websitePromo: {
     title: "Need a Website?",
-    body: "Create an account and get a website to show off your recent Youtube videos, and a story submission form."
-  }
-}
+    body: "Create an account and get a website to show off your recent Youtube videos, and a story submission form.",
+  },
+};
