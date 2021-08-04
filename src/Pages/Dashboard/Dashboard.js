00b04cafb62c3c232e28c43b2330191341358797
Tyrel Chambers <tychambers3@gmail.com>
2021-08-03 20:56:27 -0400

    remove animation class

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,71 +1,80 @@
-import React, {useEffect} from 'react'
-import DashboardNav from '../../layouts/DashboardNav/DashboardNav';
-import './Dashboard.scss';
-import Loading from '../../components/Loading/Loading';
-import { inject } from 'mobx-react';
-import { observer } from 'mobx-react-lite';
-import { getAxios } from '../../api';
-import Header from '../../layouts/Header/Header'
+import React, { useEffect } from "react";
+import DashboardNav from "../../layouts/DashboardNav/DashboardNav";
+import "./Dashboard.scss";
+import Loading from "../../components/Loading/Loading";
+import { inject } from "mobx-react";
+import { observer } from "mobx-react-lite";
+import { getAxios } from "../../api";
+import Header from "../../layouts/Header/Header";
 
-const Dashboard = inject("FormStore", "UserStore", "SiteStore", "ReadingListStore")(observer(({loading, children, UserStore, SiteStore, FormStore, ReadingListStore}) => {
-  useEffect(() => {
-    const yt = UserStore.currentUser.youtube_id;
+const Dashboard = inject(
+  "FormStore",
+  "UserStore",
+  "SiteStore",
+  "ReadingListStore"
+)(
+  observer(
+    ({
+      loading,
+      children,
+      UserStore,
+      SiteStore,
+      FormStore,
+      ReadingListStore,
+    }) => {
+      useEffect(() => {
+        const yt = UserStore.currentUser.youtube_id;
 
-    const fn = async () => {
-     
-      await getAxios({
-        url: '/site/config'
-      })
-      .then(res => {
-        if (res) {
-          SiteStore.setInitial({...res, youtube_id: res.youtube_id || yt})
-          SiteStore.setPreview({subdomain: res.subdomain})
-          FormStore.setOptionsId(res.SubmissionFormOption.uuid)
-          SiteStore.setActivated(true)
-        }
-      })
+        const fn = async () => {
+          await getAxios({
+            url: "/site/config",
+          }).then((res) => {
+            if (res) {
+              SiteStore.setInitial({
+                ...res,
+                youtube_id: res.youtube_id || yt,
+              });
+              SiteStore.setPreview({ subdomain: res.subdomain });
+              FormStore.setOptionsId(res.SubmissionFormOption.uuid);
+              SiteStore.setActivated(true);
+            }
+          });
 
-      await getAxios({
-        url: '/patreon/getUserIdentity'
-      }).then(res => {
-        if (res) {
-          UserStore.setPatron(res)
+          await getAxios({
+            url: "/patreon/getUserIdentity",
+          }).then((res) => {
+            if (res) {
+              UserStore.setPatron(res);
+            }
+          });
+        };
+        getAxios({
+          url: "/profile/reading_list?permission=true",
+        }).then((res) => {
+          if (res) {
+            ReadingListStore.addToRead(res.stories);
+          }
+        });
 
-        }
-      })
+        fn();
+      }, []);
+
+      return (
+        <div className="d-f fxd-c dashboard-wrapper">
+          <Header />
+          <DashboardNav />
+
+          {loading && <Loading />}
+
+          {!loading && (
+            <main className={`p+ dashboard-page-wrapper `}>
+              <div className="container">{children}</div>
+            </main>
+          )}
+        </div>
+      );
     }
-    getAxios({
-      url: '/profile/reading_list?permission=true'
-    }).then(res => {
-      if (res) {
-        ReadingListStore.addToRead(res.stories)
-      }
-    })
-
-    fn();
-  }, [])
-
-  return (
-    <div className="d-f fxd-c dashboard-wrapper">
-      <Header />
-      <DashboardNav />
-      
-      {loading &&
-          <Loading />
-      }
-
-      {!loading &&
-        <main className={`p+ dashboard-page-wrapper animated fadeIn faster`}>
-          <div className="container">
-            {children}
-          </div>    
-    
-        </main>
-      }
-    </div>
   )
-}));
+);
 
-
-
-export default Dashboard
+export default Dashboard;
